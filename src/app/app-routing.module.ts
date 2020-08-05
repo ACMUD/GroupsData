f709ffdec650360c6b4380674cc47651cf7f75d0
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-08-05 13:05:15 -0500

    add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: '', pathMatch: 'full', redirectTo: '' },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
